91b439446b6e7ce71379d894d7471ae7011fcb88
Derek M <43937682+DCM901108@users.noreply.github.com>
2018-12-04 11:23:38 -0500

    Revert "H2 setup"

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO APP_USER (id, username, password, role)
-VALUES (1, 'admin', 'p4ssw0rd', 'ADMIN');
-
-INSERT INTO APP_USER (id, username, password, role)
-VALUES (2, 'test-user', 'test', 'USER');
