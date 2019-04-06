1b7e401380eb74cc6057012753f991b660326e2f
Jose Rivera <joseuxs102@gmail.com>
2019-04-06 18:54:48 -0400

    Created separate class for testing methods. Working on setup

---
+++ src/test/java/com/revature/controller/AuthControllerTestJoseChris.java
@@ -0,0 +1,21 @@
+package com.revature.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.revature.service.UserService;
+
+/**
+ * This class will be used to test methods in the AuthController. Testing
+ * methods will be using Junit to test different scenarios.
+ * 
+ * @author Jose Rivera
+ * @author Christopher Shanor
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AuthControllerTestJoseChris {
+
+	@Mock
+	private UserService userService;
+}
