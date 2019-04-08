8b48a20d6dd4668110c56f4b7dcad01d490b1981
Jaitee Pitts <jaiteepitts@gmail.com>
2019-04-08 13:40:53 -0400

    added update methods

--- src/test/java/com/revature/controller/AuthControllerTest.java
+++ src/test/java/com/revature/controller/AuthControllerTest.java
@@ -1,6 +1,5 @@
 package com.revature.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -12,8 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.Authentication;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.revature.exceptions.UserNotFoundException;
 import com.revature.models.AppUser;
@@ -38,25 +37,15 @@ public class AuthControllerTest {
 	@InjectMocks
 	AuthController aControl;
 	
-	private MockMvc mmc;
 	
 	private String roleAdmin = "role_admin";
 	
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
-		this.mmc = MockMvcBuilders.standaloneSetup(aControl).build();
+		
 	}
-	
-	
-	/**
-	 * Test if MockMvc container is not null.
-	 * @throws Exception
-	 */
-	@Test
-	public void testContextLoads() throws Exception {
-	       assertThat(this.mmc).isNotNull();
-	}	
+		
 	
 	/**
 	 * Tests Delete user functionality
@@ -95,7 +84,96 @@ public class AuthControllerTest {
 		aControl.deleteUser(0);
 	}
 	
-
+//-------------------------------------------------------------------------------------------------------
+//				Test Update
+//-------------------------------------------------------------------------------------------------------
+	
+	//This test doesn't 100% follow the logic in controller method.
+	//Please refactor the controller method to use already fetched user object.
+	/**
+	 *  Supposed to test if it updates successfully with given user.
+	 */
+	@Test
+	public void testUpdateUserWithValidInfo() {
+		mockAuth.setAuthenticated(true);
+		
+		when(mockAuth.getPrincipal()).thenReturn("sally");
+		
+		when(uService.findUserByUsername(mockAuth.getPrincipal().toString())).thenReturn(backUser);
+		
+		when(mockUser.getRole()).thenReturn(roleAdmin);
+		when(mockUser.getPassword()).thenReturn("seashore newPass");
+		when(mockUser.getUsername()).thenReturn("sally");
+		when(mockUser.getFirstName()).thenReturn("salleo");
+		when(mockUser.getLastName()).thenReturn("pealle");
+		when(mockUser.getId()).thenReturn(1001);
+		
+		when(backUser.getPassword()).thenReturn("seashore");
+		when(backUser.getUsername()).thenReturn("sally");
+		when(backUser.getEmail()).thenReturn("sellsSEsasheaw@shea.shore");
+		when(backUser.getFirstName()).thenReturn("sal");
+		when(backUser.getLastName()).thenReturn("pal");
+		when(backUser.getId()).thenReturn(1000);
+		
+		when(uService.findById(mockUser.getId())).thenReturn(backUser);
+		when(uService.updateUser(mockUser)).thenReturn(true);
+		
+		aControl.updateUser(mockUser, mockAuth);
+		verify(uService, times(1)).updateUser(mockUser);
+		
+	}
+	
+	// test if some fields are empty.
+	
+	@Test(expected = UserNotFoundException.class)
+	public void testUpdateUserWithDifferentBackEndPassword() {
+		mockAuth.setAuthenticated(true);
+		
+		when(mockAuth.getPrincipal()).thenReturn("sally");
+		
+		when(uService.findUserByUsername(mockAuth.getPrincipal().toString())).thenReturn(backUser);
+		
+		when(mockUser.getRole()).thenReturn(roleAdmin);
+		when(mockUser.getPassword()).thenReturn("seashore newPass");
+		when(mockUser.getUsername()).thenReturn("sally");
+		when(mockUser.getFirstName()).thenReturn("salleo");
+		when(mockUser.getLastName()).thenReturn("pealle");
+		when(mockUser.getId()).thenReturn(1001);
+		
+		when(backUser.getPassword()).thenReturn("newPass");
+		
+		when(uService.findById(mockUser.getId())).thenReturn(backUser);
+		when(uService.updateUser(mockUser)).thenReturn(true);
+		
+		aControl.updateUser(mockUser, mockAuth);
+		verify(uService, times(1)).updateUser(mockUser);
+		
+	}
+	
+	//Pretty sure this is impossible in actual implementation
+	//backend user is fetched from the DB by the username there's no reason to check if they have the same username
+	@Test(expected = UserNotFoundException.class)
+	public void testUpdateUserWithDifferentUsername() {
+		mockAuth.setAuthenticated(true);
+		
+		when(mockAuth.getPrincipal()).thenReturn("sally");
+		
+		when(uService.findUserByUsername(mockAuth.getPrincipal().toString())).thenReturn(backUser);
+		
+		when(mockUser.getRole()).thenReturn(roleAdmin);
+		when(mockUser.getPassword()).thenReturn("seashore newPass");
+		when(mockUser.getUsername()).thenReturn("sally");
+		when(mockUser.getFirstName()).thenReturn("salleo");
+		when(mockUser.getLastName()).thenReturn("pealle");
+		when(mockUser.getId()).thenReturn(1001);
+		
+		when(backUser.getPassword()).thenReturn("seashore");
+		when(backUser.getUsername()).thenReturn("NotSally");
+		
+		aControl.updateUser(mockUser, mockAuth);
+		verify(uService, times(1)).updateUser(mockUser);
+		
+	}
 	
 	
 }
