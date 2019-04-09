af1ba0ac528370f97ac1f08d0942c091d05aaf8f
Jaitee Pitts <jaiteepitts@gmail.com>
2019-04-09 13:06:43 -0400

    added spring security test to the pom and attempted a test with a MockUser annotations

--- src/test/java/com/revature/controller/ControllerIntegrationTest.java
+++ src/test/java/com/revature/controller/ControllerIntegrationTest.java
@@ -3,14 +3,18 @@ package com.revature.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.revature.RpmAuthServiceApplication;
+import com.revature.security.ZuulConfig;
 import com.revature.service.UserService;
 
 /**
@@ -21,19 +25,55 @@ import com.revature.service.UserService;
  *
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(AuthController.class)
+//@WebMvcTest(AuthController.class)
+@WebMvcTest(RpmAuthServiceApplication.class)
 public class ControllerIntegrationTest {
 	
 	@Autowired
 	private MockMvc mvc;
 	
+	
 	@MockBean
 	private UserService userService;
 	
-	@Test
-	public void deleteUser() throws Exception{
-		this.mvc.perform(delete("/id/0")).andExpect(status().isOk());
+//	@MockBean
+//	private ZuulConfig zuulConfig;
+	
+	@Before
+	public void setup() {
+		//TODO 
+				
 	}
+	/**
+	 * Tests that delete user cannot be called without a user role
+	 * @throws Exception
+	 */
+	@Test
+	public void deleteUserUnauthorized() throws Exception{
+		this.mvc.perform(delete("/id/0")).andExpect(status().isUnauthorized());
+	}
+	/**
+	 * Tests that delete user cannot be called without a Zuul header
+	 * @throws Exception
+	 */
+	@Test
+	@WithMockUser(roles="ADMIN")
+	public void SubVersionAttemptTest() throws Exception{
+		this.mvc.perform(delete("/id/0")).andExpect(status().isUnauthorized());
+	}
+	
+	/**
+	 * 
+	 * @throws Exception
+	 */
+//	@Test
+//	@WithMockUser(roles="ADMIN")
+//	public void deleteUser() throws Exception{
+//		this.zuulConfig.getHeader();
+//		this.mvc.perform(delete("/id/1").header(zuulConfig.getHeader(), zuulConfig.getSalt()+zuulConfig.getSecret()))
+//		
+//		.andExpect(status().isOk());
+//	}
 	
 	
 	
