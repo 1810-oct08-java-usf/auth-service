3e3b127c44f887e8181207d3a9f641b75b3bd81f
Jaitee Pitts <jaiteepitts@gmail.com>
2019-04-10 22:10:26 -0400

    used value annotation to get zuul header, now test gives 415

--- src/test/java/com/revature/controller/ControllerIntegrationTest.java
+++ src/test/java/com/revature/controller/ControllerIntegrationTest.java
@@ -1,19 +1,24 @@
 package com.revature.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.revature.RpmAuthServiceApplication;
+import com.revature.security.CustomAuthenticationFilter;
 import com.revature.security.ZuulConfig;
 import com.revature.service.UserService;
 
@@ -25,25 +30,40 @@ import com.revature.service.UserService;
  *
  */
 @RunWith(SpringRunner.class)
-//@WebMvcTest(AuthController.class)
-@WebMvcTest(RpmAuthServiceApplication.class)
+@WebMvcTest(AuthController.class)
+
 public class ControllerIntegrationTest {
+	
+	private String uri = "/users";
 	
 	@Autowired
 	private MockMvc mvc;
 	
 	
+	@Value("${security.zsign.header:RPM_ZUUL_ACCESS_HEADER}")
+	private String zuulHeader;
+	@Value("${security.zsign.salt}")
+	private String salt;
+	@Value("${security.zsign.secret}")
+	private String secret;
+	
+	private String body;
+	
 	@MockBean
 	private UserService userService;
 	
-//	@MockBean
-//	private ZuulConfig zuulConfig;
+	@InjectMocks
+	private CustomAuthenticationFilter filter;
+	
+	
+	
 	
 	@Before
 	public void setup() {
-		//TODO 
-				
+		//TODO 		
+		body = filter.get_SHA_512_SecureHash(salt, secret);
 	}
+	
 	/**
 	 * Tests that delete user cannot be called without a user role
 	 * @throws Exception
@@ -51,6 +71,7 @@ public class ControllerIntegrationTest {
 	@Test
 	public void deleteUserUnauthorized() throws Exception{
 		this.mvc.perform(delete("/id/0")).andExpect(status().isUnauthorized());
+		
 	}
 	/**
 	 * Tests that delete user cannot be called without a Zuul header
@@ -66,14 +87,12 @@ public class ControllerIntegrationTest {
 	 * 
 	 * @throws Exception
 	 */
-//	@Test
-//	@WithMockUser(roles="ADMIN")
-//	public void deleteUser() throws Exception{
-//		this.zuulConfig.getHeader();
-//		this.mvc.perform(delete("/id/1").header(zuulConfig.getHeader(), zuulConfig.getSalt()+zuulConfig.getSecret()))
-//		
-//		.andExpect(status().isOk());
-//	}
+	@Test
+	@WithMockUser(roles="ADMIN")
+	public void deleteUser() throws Exception{		
+		
+		this.mvc.perform(MockMvcRequestBuilders.delete(uri+"/id/0").header(this.zuulHeader, body)).andDo(print()).andExpect(status().isOk());
+	}
 	
 	
 	
