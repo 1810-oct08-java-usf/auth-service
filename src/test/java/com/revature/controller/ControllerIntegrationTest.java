0c7f66ee6b56cb45a2f81cbace6896c2119657a0
Jaitee Pitts <jaiteepitts@gmail.com>
2019-04-14 15:54:52 -0400

    added new class without security and finished delete tests, delete when user is null gets a null pointer. Needs to be refactored

--- src/test/java/com/revature/controller/ControllerIntegrationTest.java
+++ src/test/java/com/revature/controller/ControllerIntegrationTest.java
@@ -1,7 +1,5 @@
 package com.revature.controller;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,22 +9,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.revature.models.AppUser;
 import com.revature.security.CustomAuthenticationFilter;
 import com.revature.service.UserService;
-import com.sun.jersey.api.client.ClientResponse.Status;
+
 
 /**
  * This method tests integration of the controller with MockMVC
@@ -40,22 +35,11 @@ import com.sun.jersey.api.client.ClientResponse.Status;
 
 public class ControllerIntegrationTest {
 	
-	private String uri = "/users";
-	
 	@Autowired
 	private WebApplicationContext context;
 	
 	private MockMvc mvc;
 	
-	
-	@Value("${security.zsign.header:RPM_ZUUL_ACCESS_HEADER}")
-	private String zuulHeader;
-	@Value("${security.zsign.salt}")
-	private String salt;
-	@Value("${security.zsign.secret}")
-	private String secret;
-	
-	private String body;
 	
 	@MockBean
 	private UserService userService;
@@ -71,8 +55,6 @@ public class ControllerIntegrationTest {
 	 */
 	@Before
 	public void setup() {
-		//TODO 		
-		body = filter.get_SHA_512_SecureHash(salt, secret);
 		mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(springSecurity()) 
@@ -92,7 +74,7 @@ public class ControllerIntegrationTest {
 	/**
 	 * Tests that delete user cannot be called without a Zuul header
 	 * @throws Exception
-	 * @author Jaitee Pitts (190107-Java-Spark-USF)
+	 * 
 	 */
 	@Test
 	@WithMockUser(roles= {"USER"})
@@ -100,20 +82,5 @@ public class ControllerIntegrationTest {
 		this.mvc.perform(delete("/id/0")).andExpect(status().isUnauthorized());
 	}
 	
-	/**
-	 * Tests delete User method
-	 * @throws Exception
-	 * @author Jaitee Pitts (190107-Java-Spark-USF)
-	 */
-	@Test
-	@WithMockUser(roles= {"USER","ADMIN"})
-	public void deleteUser() throws Exception{		
-		
-		this.mvc.perform(delete(uri+"/id/0").with(httpBasic("user", "password"))
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.accept(MediaType.APPLICATION_JSON_VALUE).characterEncoding("utf-8")
-				).andExpect(status().isOk());
-		
-	}
-	
+
 }
