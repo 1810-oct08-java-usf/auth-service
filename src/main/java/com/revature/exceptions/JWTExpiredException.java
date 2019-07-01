7e75bee45a9a35dadbd55fdc9ac669a8b3fdbe0c
Imbaker1234 <imbaker1234@gmail.com>
2019-07-01 11:54:00 -0400

    Added javadoc comments to JWTExpiredException.

--- src/main/java/com/revature/exceptions/JWTExpiredException.java
+++ src/main/java/com/revature/exceptions/JWTExpiredException.java
@@ -2,21 +2,32 @@ package com.revature.exceptions;
 
 public class JWTExpiredException extends RuntimeException {
 
+    /**
+     * This class, as the name implies, is to provide logic that is
+     * executed in the event of an expired JWT. It returns HTTP Status
+     * code 401 (Unauthorized) to the ng-ui which in turn results in
+     * the purge of the expired JWT and a reroute to the Login page.
+     *
+     * @author Ian Baker (1904-Apr22-Java-USF)
+     */
+
     private static final long serialVersionUID = 1L;
+
+    //TODO Add implementation to return a 401 when this exception is thrown
 
     public JWTExpiredException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
+
     }
 
     public JWTExpiredException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
+
     }
 
     public JWTExpiredException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
+
     }
 
 }
