1ba7f142f9fe3b18572a514b7b95eb83e4c02025
wsingleton <wezley.singleton@gmail.com>
2019-10-07 20:59:17 -0400

    removed unused import

--- src/main/java/com/revature/security/UserDetailsServiceImpl.java
+++ src/main/java/com/revature/security/UserDetailsServiceImpl.java
@@ -1,8 +1,6 @@
 package com.revature.security;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
