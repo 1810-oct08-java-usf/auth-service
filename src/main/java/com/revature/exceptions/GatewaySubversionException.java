9e4278cbfe5fbee26725c101ae697e1943c4f336
wsingleton <wezley.singleton@gmail.com>
2019-10-07 20:19:20 -0400

    refactor exceptions
    JwtExpiredException completely replaced by GatewaySubversionException
    (formerly known as the SubversionAttemptException). The only
    use of this exception was in the GatewaySubversionFilter (formerly known
    as the CustomAuthenticationFilter).

--- src/main/java/com/revature/exceptions/GatewaySubversionException.java
+++ src/main/java/com/revature/exceptions/GatewaySubversionException.java
@@ -2,21 +2,21 @@ package com.revature.exceptions;
 
 import java.io.IOException;
 
-public class SubversionAttemptException extends IOException {
+public class GatewaySubversionException extends IOException {
 
 	private static final long serialVersionUID = -1736687159994666796L;
 
-	public SubversionAttemptException(String message, Throwable cause) {
+	public GatewaySubversionException(String message, Throwable cause) {
 		super(message, cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public SubversionAttemptException(String message) {
+	public GatewaySubversionException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 
-	public SubversionAttemptException(Throwable cause) {
+	public GatewaySubversionException(Throwable cause) {
 		super(cause);
 		// TODO Auto-generated constructor stub
 	}
