e5a60cebc962a1ade0d82d5a3919a4f40e634391
wsingleton <wezley.singleton@gmail.com>
2019-10-07 20:42:25 -0400

    refactor
    major changes made:
    - removed validatePublicHeader method as its logic was identical to the validateHeader method
    - renamed hashing method in accordance with proper naming conventions - new name: getHash
    - removed salt param from getHash param list since this value was easily obtainable from the zuulConfig instance member
    - simplified logic of doFilter, simply flagging requests with the gateway-subverted (bool) and request-type (string) attributes and rejecting resource requests that subvert the gateway

--- src/main/java/com/revature/security/GatewaySubversionFilter.java
+++ src/main/java/com/revature/security/GatewaySubversionFilter.java
@@ -1,9 +1,9 @@
 package com.revature.security;
 
-import com.revature.exceptions.JWTExpiredException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,140 +11,113 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.revature.exceptions.GatewaySubversionException;
+
+/*
+ * TODO
+ * 	- Obtain IP address of subverting requesters for logging
+ * 	- Log all exceptions appropriately
+ */
 
 /**
  * The purpose of this class is to create custom filters for this service to only
- * accept and authorize requests from zuul or to anything that wants info about the
- * health of the service. Currently, it looks for this header from zuul using the ZuulConfig.
- * object. If anything tries to hit an end point for actuator it lets it through regardless
- * of headers. This is allowed for now as ELB on AWS will check actuator/info in order
- * to evaluate the health of the service. If it does not get through to that end point,
- * ELB will shut down the service and start a new one within 3 minutes.
- *
- * @author Sean Doyle (1810-Oct22-Java-USF)
- * @author Josh Jibilian (1810-Oct22-Java-USF)
- * @author Colt Ossoff (1810-Oct22-Java-USF)
- * @author Bronwen Hughes (1810-Oct22-Java-USF)
- * @author John Savath (1810-Oct22-Java-USF)
+ * accept and authorize resource requests from the gateway. This is accomplished by 
+ * looking for the value of the header from named RPM_ZUUL_ACCESS_HEADER, which is 
+ * applied by the gateway when the request is first intercepted.
  */
-public class CustomAuthenticationFilter extends GenericFilterBean {
-
+public class GatewaySubversionFilter extends GenericFilterBean {
+	
 	private ZuulConfig zuulConfig;
 
 	/**
 	 * Constructor for CustomAuthenticationFilter that instantiates the ZuulConfig
 	 * field.
 	 *
-	 * @param zuulConfig Provides configuration for validating that requests came through Zuul
+	 * @param zuulConfig 
+	 * 		Provides configuration for validating that requests came through Zuul
 	 */
-	public CustomAuthenticationFilter(ZuulConfig zuulConfig) {
+	public GatewaySubversionFilter(ZuulConfig zuulConfig) {
 		this.zuulConfig = zuulConfig;
 	}
 
-	/**
-	 * This is the filter that is used to authenticate specific traffic.
-	 *
-	 * @author Sean Doyle (1810-Oct22-Java-USF)
-	 */
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
-			throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		String headerZuul = httpRequest.getHeader(zuulConfig.getHeader());
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest httpReq = (HttpServletRequest) req;
+		HttpServletResponse httpResp = (HttpServletResponse) resp;
+		
+		String headerZuul = httpReq.getHeader(zuulConfig.getHeader());
+		
 		try {
-			//System.out.println(httpRequest.getRequestURI());
-			// This is where the main check if the actuator end point is being hit or
-			//	that the zuul header is present.
 
-			if (httpRequest.getRequestURI().contains("/actuator")) {
-				Authentication auth = new AccessAuthenticationToken(headerZuul, "ROLE_ACTUATOR", new ArrayList<>());
-				SecurityContextHolder.getContext().setAuthentication(auth);
+			if (httpReq.getRequestURI().contains("/actuator")) {
+				httpReq.setAttribute("gateway-subverted", true);
+				httpReq.setAttribute("request-type", "health-check");
 			} else if (validateHeader(headerZuul)) {
-				Authentication auth = new AccessAuthenticationToken(headerZuul, "ROLE_ADMIN", new ArrayList<>());
-				SecurityContextHolder.getContext().setAuthentication(auth);
-			} else if (validatePublicHeader(headerZuul)) {
-				Authentication auth = new AccessAuthenticationToken(headerZuul, "ROLE_USER", new ArrayList<>());
-				SecurityContextHolder.getContext().setAuthentication(auth);
+				httpReq.setAttribute("gateway-subverted", false);
+				httpReq.setAttribute("request-type", "resource");
 			} else {
-				/*
-				 * In case of attempted subversion around Zuul we want to invalidate the
-				 * session, so we can guarantee that the user will not be authenticated
-				 */
+				httpReq.setAttribute("gateway-subverted", true);
+				httpReq.setAttribute("request-type", "resource");
 				SecurityContextHolder.clearContext();
-				((HttpServletResponse) response).setStatus(401);
-				// Log this
-//				throw new SubversionAttemptException("ZUUL header is " + headerZuul);
-				throw new JWTExpiredException("ZUUL header is " + headerZuul);
+				httpResp.setStatus(401);
+				throw new GatewaySubversionException("No gateway header found on request.");
 			}
-//		} catch (SubversionAttemptException e) {
-		} catch (JWTExpiredException jwtee) {
-			/*
-			 * TODO This should be refactored to log the failed authentication attempt,
-			 * including the IP address of the requester.
-			 */
-			jwtee.printStackTrace();
-			String ipAddress = ((HttpServletRequest) request).getHeader("X-FORWARDED-FOR");
-			if (ipAddress == null) {
-				ipAddress = request.getRemoteAddr();
-			}
-
-			String appUrl = request.getScheme() + "://" + request.getLocalAddr();
-			System.out.println("URL     " + appUrl);
-			// throw new RuntimeException(ipAddress + " " + appUrl, e);
+			
+		} catch (GatewaySubversionException gse) {
+			gse.printStackTrace();
 		}
-		// Activates the next filter if there is any.
-		filterChain.doFilter(request, response);
+		
+		chain.doFilter(httpReq, httpResp);
+
 	}
 
 	/**
-	 * Method to perform a SHA-512 hash. Retrieved from:
-	 * {@link https://stackoverflow.com/questions/33085493/how-to-hash-a-password-with-sha-512-in-java}
+	 * Method to perform a SHA-512 hash.
+	 * 
+	 * @param password
+	 * 		Represents the password which will be hashed
 	 */
-	public String get_SHA_512_SecureHash(String passwordToHash, String salt) {
+	public String getHash(String password) {
+		
 		String generatedPassword = null;
+		
 		try {
+			
 			MessageDigest md = MessageDigest.getInstance("SHA-512");
-			md.update(salt.getBytes(StandardCharsets.UTF_8));
-			byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+			md.update(zuulConfig.getSalt().getBytes(StandardCharsets.UTF_8));
+			byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
 			StringBuilder sb = new StringBuilder();
+		
 			for (int i = 0; i < bytes.length; i++) {
 				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
 			}
+			
 			generatedPassword = sb.toString();
+			
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
+		
 		return generatedPassword;
 	}
 
 	/**
-	 * Method to check if the header matches that provided by Zuul
+	 * Method to check if the header value matches the expected value, using the ZuulConfig object.
 	 *
-	 * @param header The retrieved header from the request object
+	 * @param header 
+	 * 		The retrieved header from the request object
 	 */
 	public boolean validateHeader(String header) {
 		if (header == null) {
 			return false;
 		}
-		return header.equals(get_SHA_512_SecureHash(zuulConfig.getSecret(), zuulConfig.getSalt()));
+		return header.equals(getHash(zuulConfig.getSecret()));
 		
 	}
 
-	/**
-	 * Method to check if the header matches that provided by Zuul, for public access only
-	 *
-	 * @param header The retrieved header from the request object
-	 */
-	public boolean validatePublicHeader(String header) {
-		if (header == null) {
-			return false;
-		}
-		return header.equals(get_SHA_512_SecureHash(zuulConfig.getSalt(), zuulConfig.getSecret()));
-	}
 }
