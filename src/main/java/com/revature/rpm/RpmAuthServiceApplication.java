cb91403cf12cc00a1129d334e0aa5188a706661e
ElderFuthark <40003119+ElderFuthark@users.noreply.github.com>
2019-10-24 23:06:07 -0400

    Driver of auth-service documented and Googleified
    Documentation above the annotations and method explain the intricacies
    of both. The former explains that @SpringBootApplication is a meta
    annotation that implies three others necessary for making a Spring Boot
    application. The latter explains what SpringApplication.run() does.
    
    Google's Java Style has been applied to this file.

--- src/main/java/com/revature/rpm/RpmAuthServiceApplication.java
+++ src/main/java/com/revature/rpm/RpmAuthServiceApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Configures this app to be a Spring Boot application (implies @component, @EnableAutoConfiguration
+ * and @ComponentScan) and a client of the Eureka discovery service. The only method, main, runs
+ * this app as a Spring application with passed in parameters
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class RpmAuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RpmAuthServiceApplication.class, args);
-	}
-
+  /**
+   * Bootstrap and launch application and return the running application context.
+   *
+   * @param args - an array of Strings corresponding to arguments from the command line.
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(RpmAuthServiceApplication.class, args);
+  }
 }
