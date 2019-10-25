4d347aa9625a490638b1a78f48f36d75b00ee84a
ElderFuthark <40003119+ElderFuthark@users.noreply.github.com>
2019-10-25 14:01:10 -0400

    exceptions package documented and Googleified
    All classes, constructors and methods have been documented with
    javadocs. All descriptions are brief but fully describe functionality.
    Parameters are appropriately described using the @param annotation.
    
    All files also comply with Google's Java Style Guide.

--- src/main/java/com/revature/rpm/exceptions/UserUpdateException.java
+++ src/main/java/com/revature/rpm/exceptions/UserUpdateException.java
@@ -1,11 +1,20 @@
 package com.revature.rpm.exceptions;
 
+/**
+ * Extends RuntimeException and allows for a custom exception to be thrown when updating an existing
+ * user.
+ */
 public class UserUpdateException extends RuntimeException {
 
-	private static final long serialVersionUID = -1759991348700306860L;
+  private static final long serialVersionUID = -1759991348700306860L;
 
-	public UserUpdateException(String message) {
-		super(message);
-	}
-	
+  /**
+   * A constructor that creates a UserUpdateException by passing a custom message to the parent's
+   * appropriate constructor.
+   *
+   * @param message - A custom message to print with the exception.
+   */
+  public UserUpdateException(String message) {
+    super(message);
+  }
 }
