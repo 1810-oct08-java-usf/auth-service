f615dcd4c792da6987b0ce7c2306e41cd80ad2e1
Wezley Singleton <wezley.singleton@gmail.com>
2019-10-29 16:24:08 -0400

    added constructor

--- src/main/java/com/revature/rpm/dtos/UserCredentials.java
+++ src/main/java/com/revature/rpm/dtos/UserCredentials.java
@@ -10,6 +10,15 @@ public class UserCredentials {
 
 	private String username;
 	private String password;
+	
+	public UserCredentials() {
+		super();
+	}
+	
+	public UserCredentials(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
 
 	public String getUsername() {
 		return username;
