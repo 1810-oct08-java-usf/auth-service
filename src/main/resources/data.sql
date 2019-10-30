769fb4855bd2314d112aa67a4c76efca553caa5f
Wezley Singleton <wezley.singleton@gmail.com>
2019-10-30 11:46:39 -0400

    update data.sql to use hashed passwords for dummy users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO rpm_users (rpm_user_id, rpm_user_fn, rpm_user_ln, rpm_user_email, rpm_user_username, rpm_user_pw, role)
-VALUES (1, 'Adam', 'Inn', 'admin@revature.com', 'admin', 'p4ssw0rd', 'ROLE_ADMIN');
+VALUES (1, 'Adam', 'Inn', 'admin@revature.com', 'admin', '$2a$10$Uu4u85.lOVnnMubYoKA1A.V3fEowguP2EsaaSgh.XRpPsJ2pRqLIi', 'ROLE_ADMIN');
 
 INSERT INTO rpm_users (rpm_user_id, rpm_user_fn, rpm_user_ln, rpm_user_email, rpm_user_username, rpm_user_pw, role)
-VALUES (2, 'Tester', 'McTesterson', 'test@revature.com', 'test-user', 'test', 'ROLE_USER');
+VALUES (2, 'Tester', 'McTesterson', 'test@revature.com', 'test-user', '$2a$10$E3aHhhdDJGTqRqQmk8cEWethpzRvRAOzRhU6tSXjguiT2NlLBKbn.', 'ROLE_USER');
